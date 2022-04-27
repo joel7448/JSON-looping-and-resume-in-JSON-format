f2d870101e1e752f21b4e391489dcf16d3de1513
joel7448 <103260485+joel7448@users.noreply.github.com>
2022-04-27 19:33:42 +0530

    TASK DAY3

---
+++ JSONresume.js
@@ -0,0 +1,18 @@
+var resume = {                        //JSON FORMAT
+    "name" : "A.Joel" , 
+    "Education qualification":"BE Mechanical engineering",
+     "CGPA" : "7.6",
+     "Name of institution of completion of UG":"SVCE",
+     "Work experiences" : "Marketing at Polyhose , Intern at Royal enfield ",
+     "Extracuricular activities" : "Programming problems , playing chess , Walking",
+     "Address":"K2,classic , RC edencrest",
+     "Contact number":"7448797356",
+     "LinkedIn": "joel-a",
+     "Naukri" : "A Joel",
+}
+for( i in resume){
+
+    console.log(`${i}`);
+
+}
+
